d768ef00edc3835e179c02813b0afeaefa40d04e
Lisa <yalisagit@gmail.com>
2019-12-07 19:09:05 +0300

    funny comments

--- 2019_12_07/script_rvest.r
+++ 2019_12_07/script_rvest.r
@@ -4,7 +4,7 @@
 library(rvest)
 page <- read_html('https://ege.hse.ru/rating/2019/81031971/all/?rlist=&ptype=0&vuz-abiturients-budget-order=ge&vuz-abiturients-budget-val=10')
 
-
+          /html/body/div/div/div[2]/div[2]/div/div/ul[1]/li[2]/ul/li[1]/a
 #xpth = '/html/body/div/div/div[2]/div[2]/div/div/ul[1]/li[5]/ul/li[1]'
 
 links <- page %>%
@@ -12,8 +12,8 @@ links <- page %>%
   html_nodes('li') %>% 
   html_nodes('ul') %>% 
   html_nodes('li') %>%
-  html_nodes('a') %>% 
-  html_attr('href')
+  html_nodes('a') %>% # "господа, мы пробили дно"
+  html_attr('href') # но, какие-то ссылки полные, а какие-то нет... нужна предобработка "Это просто безалаберность, на мой взгляд" =)
 
 # получили таблицу, но мы же хочи все таблицы!
 # собрать ссылки и пройтись по ним циклом
@@ -30,7 +30,7 @@ for (i in links[1:5]){
     page <- read_html(url)
     df <- page %>% html_table()
     l[[ii]] <- as.data.frame(df[[2]])
-    ii <- ii + 1
+    ii <- ii + 1 # anumerate в питоне (?)
   } else { 
     page <- read_html(i)
     df <- page %>% html_table()
@@ -39,7 +39,7 @@ for (i in links[1:5]){
     }
 }
 
-fin <- dplyr::bind_rows(l)
+fin <- dplyr::bind_rows(l) # dim(fin) ... и видим, что опять нужна предобработка...
 
 names(fin)
 str(fin)
@@ -50,6 +50,7 @@ dim(fin)
 
 
 library("RSelenium")
+# Selenium - эмулиреут дейсвтия клиента, нужне для проверки работоспособности
 
 driver<- rsDriver()
 remDr <- driver[["client"]]

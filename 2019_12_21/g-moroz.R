7bfb82d027104f840bdbdf1efbdf0dc593c69727
Lisa <yalisagit@gmail.com>
2019-12-21 14:23:40 +0300

    new day new script

---
+++ 2019_12_21/g-moroz.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+https://agricolamz.github.io/2019.12.21_EU_purrr/index.html
+
+got_chars <- jsonlite::read_json("https://raw.githubusercontent.com/agricolamz/2019.12.21_EU_purrr/master/data/got_chars.json")
+got_chars[1]

f7b6cc8f992f193ea2d4694b76dd065a7e79804b
Lisa <yalisagit@gmail.com>
2019-12-21 15:12:04 +0300

    plot -- done

--- 2019_12_21/g-moroz.R
+++ 2019_12_21/g-moroz.R
@@ -4,3 +4,26 @@ https://agricolamz.github.io/2019.12.21_EU_purrr/index.html
 
 got_chars <- jsonlite::read_json("https://raw.githubusercontent.com/agricolamz/2019.12.21_EU_purrr/master/data/got_chars.json")
 got_chars[1]
+listviewer::jsonedit
+
+
+got_chars %>% 
+  map("gender") %>% 
+  unlist() %>% 
+  table()
+
+
+dict <- read_tsv("https://raw.githubusercontent.com/agricolamz/DS_for_DH/master/data/freq_dict_2011.csv")
+
+dict$lemma %>%
+  str_split("") %>% 
+  unlist() %>% 
+  enframe() %>% 
+  mutate(value = str_to_lower(value)) %>% 
+  filter(str_detect(value, "\\w")) %>% 
+  count(value) %>% 
+  ggplot(aes(x = fct_reorder(value, n), y = n))+
+  geom_col()+
+  coord_flip()+
+  labs(x = NULL, y = NULL, caption = "данные из [Ляшевская, Шаров 2011]")
+  
